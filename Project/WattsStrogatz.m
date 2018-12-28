38c9af57bc296781dcbfb4c694bdf602757bb906
Toby <TobyYoung0412@users.noreply.github.com>
2018-12-28 09:54:21 +0800

    修改了WS注释

--- Project/WattsStrogatz.m
+++ Project/WattsStrogatz.m
@@ -1,16 +1,17 @@
 function h = WattsStrogatz(N,K,beta)
-% H = WattsStrogatz(N,K,beta) returns a Watts-Strogatz model graph with N
-% nodes, N*K edges, mean node degree 2*K, and rewiring probability beta.
-%
-% beta = 0 is a ring lattice, and beta = 1 is a random graph.
+% H = WattsStrogatz(N,K,beta)返回由N个节点，N*K条边，节点度数2*K构造，
+% 以beta概率重连边得到的WS模型
+% beta = 0为圆形，beta = 1为随机图
 
-% Connect each node to its K next and previous neighbors. This constructs
-% indices for a ring lattice.
-s = repelem((1:N)',1,K);
-t = s + repmat(1:K,N,1);
+% 连接每个节点和他后向K临近的节点，该步骤构造结果为圆形
+s = repelem((1:N)',1,K); % 源节点矩阵
+t = s + repmat(1:K,N,1); % K临近目标节点矩阵，并做调整
 t = mod(t-1,N)+1;
 
-% Rewire the target node of each edge with probability beta
+% 以beta概率重新连接每个节点对应的目标节点
+% 先确定需要重连的边
+% 在确定新连接的边(保证不重复)
+% 在生成的网路上进行操作
 for source=1:N    
     switchEdge = rand(K, 1) < beta;
     
@@ -21,10 +22,8 @@ for source=1:N
     
     [~, ind] = sort(newTargets, 'descend');
     t(source, switchEdge) = ind(1:nnz(switchEdge));
-    
-    temp = graph(s,t);
-    plot(temp,'NodeColor','k','EdgeAlpha',0.1);
 end
 
+% 将生成的网络转化为满邻接矩阵
 h = full(adjacency(graph(s,t)));
 end
